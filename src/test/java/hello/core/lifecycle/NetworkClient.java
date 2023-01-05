7b83091cc8fdad31271e9492c03c0a8a3458b695
jjdone <ehdlfaos@gmail.com>
2023-01-05 15:05:49 +0900

    test: 인터페이스를 활용한 빈 등록 초기화, 소멸

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,15 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import net.bytebuddy.agent.builder.AgentBuilder;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -26,5 +28,22 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close = " + url);
+    }
+
+    /**
+     * 의존 관계 주입이 끝날 때 호출되는 메서드이다.
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    /**
+     * 빈 소멸 전에 호출되는 메서드이다.
+     */
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
     }
 }
